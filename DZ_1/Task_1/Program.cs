343094aa2d29d530e31a7da3d63e8d135ed48c9f
Aleksey <pilot-1981@yandex.ru>
2022-08-25 08:47:37 +0500

    add insert Task_1

---
+++ DZ_1/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1 = "); 
+int num1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите число 2 = "); 
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 > num1)
+{
+    Console.WriteLine ("Из двух чисел большее число " + num2);
+    Console.WriteLine ("а меньшее число " + num1);
+}
+else
+{
+    Console.WriteLine ("Из двух чисел большее число " + num1);
+    Console.WriteLine ("а меньшее число " + num2);
+}
