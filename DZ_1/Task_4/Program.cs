ebeb8221b184c38765a8cc19a4505f9e6e7e12c0
Aleksey <pilot-1981@yandex.ru>
2022-08-25 11:40:19 +0500

    add Task_4

--- DZ_1/Task_4/Program.cs
+++ DZ_1/Task_4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите произвольное число = "); 
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Получим массив следующих четных чисел: ");
+
+for (int i=1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(" " + i);
+    }
+}
